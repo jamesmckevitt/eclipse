70fcfcb4303600c4d6c5a6a042b110e1563c9c82
jamesmckevitt <james.mckevitt@outlook.com>
2026-04-11 14:40:30 +0200

    require Python 3.10 due to PEP 604 syntax usage

--- setup.py
+++ setup.py
@@ -35,12 +35,10 @@ setup(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Astronomy",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     install_requires=[
         "numpy",
         "astropy",
